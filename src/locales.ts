fd334d63825b6c31ae68852a3628189f40f6e898
白熱 <sonne@asaki.me>
2024-02-02 14:28:06 +0800

    update demo

---
+++ src/locales.ts
@@ -0,0 +1,21 @@
+import { LocaleType } from "@univerjs/core";
+import { enUS as UniverDesignEnUS } from "@univerjs/design";
+import { enUS as UniverDocsUIEnUS } from "@univerjs/docs-ui";
+import { enUS as UniverSheetsEnUS } from "@univerjs/sheets";
+import { enUS as UniverSheetsUIEnUS } from "@univerjs/sheets-ui";
+import { enUS as UniverUiEnUS } from "@univerjs/ui";
+import { functionEnUS, functionZhCN } from "./custom-function";
+
+export const locales = {
+  [LocaleType.EN_US]: {
+    ...UniverSheetsEnUS,
+    ...UniverDocsUIEnUS,
+    ...UniverSheetsUIEnUS,
+    ...UniverUiEnUS,
+    ...UniverDesignEnUS,
+    ...functionEnUS,
+  },
+  [LocaleType.ZH_CN]: {
+    ...functionZhCN,
+  },
+};
---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import "./style.css";
+import "@univerjs/design/lib/index.css";
+import "@univerjs/ui/lib/index.css";
+import "@univerjs/sheets-ui/lib/index.css";
+import "@univerjs/sheets-formula/lib/index.css";
+import "@univerjs/sheets-numfmt/lib/index.css";
+
+import { LocaleType, LogLevel, Univer } from "@univerjs/core";
+import { defaultTheme } from "@univerjs/design";
+import { UniverDocsPlugin } from "@univerjs/docs";
+import { UniverFormulaEnginePlugin } from "@univerjs/engine-formula";
+import { UniverRenderEnginePlugin } from "@univerjs/engine-render";
+import { UniverSheetsPlugin } from "@univerjs/sheets";
+import { UniverSheetsFormulaPlugin } from "@univerjs/sheets-formula";
+import { UniverSheetsNumfmtPlugin } from "@univerjs/sheets-numfmt";
+import { UniverSheetsUIPlugin } from "@univerjs/sheets-ui";
+import { UniverUIPlugin } from "@univerjs/ui";
+import { FUNCTION_LIST_USER, functionUser } from "./custom-function";
+import { locales } from "./locales";
+
+// univer
+const univer = new Univer({
+  theme: defaultTheme,
+  locale: LocaleType.ZH_CN,
+  locales,
+  logLevel: LogLevel.VERBOSE,
+});
+
+// core plugins
+univer.registerPlugin(UniverDocsPlugin, {
+  hasScroll: false,
+});
+univer.registerPlugin(UniverRenderEnginePlugin);
+univer.registerPlugin(UniverUIPlugin, {
+  container: "app",
+  header: true,
+  toolbar: true,
+  footer: true,
+});
+univer.registerPlugin(UniverSheetsPlugin);
+univer.registerPlugin(UniverSheetsUIPlugin);
+
+// sheet feature plugins
+univer.registerPlugin(UniverSheetsNumfmtPlugin);
+univer.registerPlugin(UniverFormulaEnginePlugin, {
+  function: functionUser,
+});
+univer.registerPlugin(UniverSheetsFormulaPlugin, {
+  description: FUNCTION_LIST_USER,
+});
+
+// create univer sheet instance
+univer.createUniverSheet({});
